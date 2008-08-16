5568c9c71bcf975fb892459372326f937d397e46
Joshua Peek <josh@joshpeek.com>
2008-08-16 11:32:24 -0500

    tasks for listing apache sites and modules

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -24,14 +24,34 @@ namespace :apache do
     sudo "/etc/init.d/apache2 force-reload"
   end
 
-  desc "Disable Apache vhost"
+  desc "List enabled Apache sites"
+  task :enabled_sites, :roles => :web do
+    run "ls /etc/apache2/sites-enabled"
+  end
+
+  desc "List available Apache sites"
+  task :available_sites, :roles => :web do
+    run "ls /etc/apache2/sites-available"
+  end
+
+  desc "List enabled Apache modules"
+  task :enabled_modules, :roles => :web do
+    run "ls /etc/apache2/mods-enabled"
+  end
+
+  desc "List available Apache modules"
+  task :available_modules, :roles => :web do
+    run "ls /etc/apache2/mods-available"
+  end
+
+  desc "Disable Apache site"
   task :disable_site, :roles => :web do
     site = Capistrano::CLI.ui.ask("Which site should we disable: ")
     sudo "sudo a2dissite #{site}"
     reload
   end
 
-  desc "Enable Apache vhost"
+  desc "Enable Apache site"
   task :enable_site, :roles => :web do
     site = Capistrano::CLI.ui.ask("Which site should we enable: ")
     sudo "sudo a2ensite #{site}"
