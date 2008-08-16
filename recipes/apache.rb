c33103fbac4ea2231b786de645e475f45031af5e
Joshua Peek <josh@joshpeek.com>
2008-08-16 11:22:35 -0500

    Add some apache a2ensite and a2dissite tasks

--- recipes/apache.rb
+++ recipes/apache.rb
@@ -18,4 +18,37 @@ namespace :apache do
   task :reload, :roles => :web do
     sudo "/etc/init.d/apache2 reload"
   end
+
+  desc "Force reload Apache webserver"
+  task :force_reload, :roles => :web do
+    sudo "/etc/init.d/apache2 force-reload"
+  end
+
+  desc "Disable Apache vhost"
+  task :disable_site, :roles => :web do
+    site = Capistrano::CLI.ui.ask("Which site should we disable: ")
+    sudo "sudo a2dissite #{site}"
+    reload
+  end
+
+  desc "Enable Apache vhost"
+  task :enable_site, :roles => :web do
+    site = Capistrano::CLI.ui.ask("Which site should we enable: ")
+    sudo "sudo a2ensite #{site}"
+    reload
+  end
+
+  desc "Disable Apache module"
+  task :disable_module, :roles => :web do
+    mod = Capistrano::CLI.ui.ask("Which module should we disable: ")
+    sudo "sudo a2dismod #{mod}"
+    force_reload
+  end
+
+  desc "Enable Apache module"
+  task :enable_module, :roles => :web do
+    mod = Capistrano::CLI.ui.ask("Which module should we enable: ")
+    sudo "sudo a2enmod #{mod}"
+    force_reload
+  end
 end
