da231d8b6c11d3702e8a84d15972be3b2ce7dbf2
Joshua Peek <josh@joshpeek.com>
2008-09-01 18:39:26 -0500

    Document mysql tasks

--- lib/capistrano/ext/slicehost/mysql.rb
+++ lib/capistrano/ext/slicehost/mysql.rb
@@ -1,4 +1,5 @@
 namespace :mysql do
+  desc "Install MySQL"
   task :install, :roles => :db do
     sudo "sudo aptitude install -y mysql-server mysql-client libmysqlclient15-dev libmysql-ruby1.8"
   end
