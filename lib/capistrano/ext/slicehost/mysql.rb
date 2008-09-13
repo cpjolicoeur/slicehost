3bea94a4770cab50dc49da669b925ce570c85710
Joshua Peek <josh@joshpeek.com>
2008-09-13 17:37:01 -0500

    Some MySQL server commands

--- lib/capistrano/ext/slicehost/mysql.rb
+++ lib/capistrano/ext/slicehost/mysql.rb
@@ -1,6 +1,22 @@
 namespace :mysql do
+  desc "Restarts MySQL database server"
+  task :restart, :roles => :db do
+    sudo "/etc/init.d/mysql restart"
+  end
+
+  desc "Starts MySQL database server"
+  task :start, :roles => :db do
+    sudo "/etc/init.d/mysql start"
+  end
+
+  desc "Stops MySQL database server"
+  task :stop, :roles => :db do
+    sudo "/etc/init.d/mysql stop"
+  end
+
   desc "Install MySQL"
   task :install, :roles => :db do
-    sudo "sudo aptitude install -y mysql-server mysql-client libmysqlclient15-dev libmysql-ruby1.8"
+    sudo "aptitude install -y mysql-server mysql-client libmysqlclient15-dev"
+    sudo "aptitude install -y libmysql-ruby1.8"
   end
 end
